31f7d10018b34eea8af300122e87abac31252292
Rishikesh Kumar <rishikesh.f.kumar@accenture.com>
2025-10-14 17:01:45 +0530

    sfd

---
+++ infrastrcture/main.tf
@@ -0,0 +1,80 @@
+
+# Create a VPC
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/20"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.16.0/20"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.32.0/20"
+  availability_zone       = "us-east-1d"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "internet_gw" {
+  vpc_id                  = aws_vpc.main.id
+
+}
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+}
+
+
+  module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "my-devops-project"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id                   = aws_vpc.main.id
+  subnet_ids               = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+  control_plane_subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+
+  eks_managed_node_groups = {
+    green = {
+      min_size       = 1
+      max_size       = 1
+      desired_size   = 1
+      instance_types = ["t3.medium"]
+    }
+  }
+}
+
+
+
+} 
---
+++ infrastrcture/modules/vpc/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "my-coll-vpc-for-modules"
+}
