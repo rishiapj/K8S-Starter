7ce552604e36207998969ae3c50187339856c821
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 17:07:13 +0000

    Updated providers.tf

--- infrastrcture/providers.tf
+++ infrastrcture/providers.tf
@@ -12,10 +12,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "rishi-terraform-bucket-985809756777" # change this
+backend "s3" {
+    bucket         = "rishi-terraform-bucket-985809756777"
     key            = "rishi/terraform.tfstate"
-    region         = "us-east-1"  
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table" # optional for state locking
   }
-}
