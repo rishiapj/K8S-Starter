a3951209a26da3f7b198df420fb8c838a43eba8a
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 20:10:01 +0000

    Updated main.tf

--- infrastrcture-destroy/main.tf
+++ infrastrcture-destroy/main.tf
@@ -1,9 +1,21 @@
-
 terraform {
+  required_version = ">= 1.3.0"
+
   backend "s3" {
     bucket         = "rishi-terraform-bucket-985809756777"
     key            = "rishi/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table" # optional for state locking
+    encrypt        = true
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # Use the latest stable version
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
