f33052103810c10d4774bb29f1a9fd52159b9f6f
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 17:57:22 +0000

    Updated providers.tf

--- infrastrcture/providers.tf
+++ infrastrcture/providers.tf
@@ -7,14 +7,25 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
+terraform {
+  required_version = ">= 1.3.0"
 
-backend "s3" {
+  backend "s3" {
     bucket         = "rishi-terraform-bucket-985809756777"
     key            = "rishi/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table" # optional for state locking
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
