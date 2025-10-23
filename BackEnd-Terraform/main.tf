54329b904d9a182dff69bf189360a7efe0cec744
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 17:53:53 +0000

    Updated main.tf

--- BackEnd-Terraform/main.tf
+++ BackEnd-Terraform/main.tf
@@ -2,25 +2,28 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "rishi-terraform-bucket-985809756777" # change this  
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "rishi-terraform-bucket-985809756777"
   force_destroy = true
 
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "bootstrap"
+  }
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
     type = "S"
   }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "bootstrap"
+  }
 }
-
-    
-
-    
