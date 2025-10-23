b18fa6bc70ac0db7f5d19e53bff35f5e0acf8bcb
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 18:23:34 +0000

    Updated main.tf

--- BackEnd-Terraform/main.tf
+++ BackEnd-Terraform/main.tf
@@ -11,19 +11,3 @@ resource "aws_s3_bucket" "terraform_state" {
     Environment = "bootstrap"
   }
 }
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform Lock Table"
-    Environment = "bootstrap"
-  }
-}
