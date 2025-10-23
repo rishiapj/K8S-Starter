c91257c9ddc8c1dc45c4f9ae15e321b3c93a7d18
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 17:02:26 +0000

    Updated main.tf

--- BackEnd-Terraform/main.tf
+++ BackEnd-Terraform/main.tf
@@ -4,7 +4,10 @@
 
     resource "aws_s3_bucket" "s3_bucket" {
       bucket = "rishi-terraform-bucket-985809756777" # Choose a globally unique name
-
+      key            = "rishi/terraform.tfstate"
+      region         = "us-east-1"
+      encrypt        = true
+      dynamodb_table = "terraform-lock"
       tags = {
         Name        = "Terraform State Bucket"
         Environment = "Dev"
