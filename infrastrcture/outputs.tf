24db340cc638d9e45c8de31fadd596fdfa2a3a14
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 12:09:39 +0000

    Updated outputs.tf

--- infrastrcture/outputs.tf
+++ infrastrcture/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = [
+    aws_subnet.subnet_1.id,
+    aws_subnet.subnet_2.id,
+    aws_subnet.subnet_3.id
+  ]
 }
