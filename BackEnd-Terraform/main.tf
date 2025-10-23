9c5ef75aca3722bd068dbd487b31a313d2c35293
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 14:50:12 +0000

    Updated main.tf

--- BackEnd-Terraform/main.tf
+++ BackEnd-Terraform/main.tf
@@ -11,18 +11,4 @@
       }
     }
 
-    resource "aws_dynamodb_table" "terraform_state_lock" {
-      name         = "terraform-state-lock-table" # Choose a unique name
-      billing_mode = "PAY_PER_REQUEST"
-      hash_key     = "LockID"
-
-      attribute {
-        name = "LockID"
-        type = "S"
-      }
-
-      tags = {
-        Name        = "Terraform State Lock Table"
-        Environment = "Dev"
-      }
-    }
+    
