dd4d3e0aba082c0efd2d21e28d00a39c75f53fea
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-23 13:13:07 +0000

    Added main.tf

---
+++ BackEnd-Terraform/main.tf
@@ -0,0 +1,41 @@
+    provider "aws" {
+      region = "us-east-1" # Replace with your desired region
+    }
+
+    resource "aws_s3_bucket" "terraform_state_bucket" {
+      bucket = "terraform-state-bucket-name" # Choose a globally unique name
+      acl    = "private"
+
+      versioning {
+        enabled = true # Recommended for state file versioning
+      }
+
+      server_side_encryption_configuration {
+        rule {
+          apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+          }
+        }
+      }
+
+      tags = {
+        Name        = "Terraform State Bucket"
+        Environment = "Dev"
+      }
+    }
+
+    resource "aws_dynamodb_table" "terraform_state_lock" {
+      name         = "terraform-state-lock-table" # Choose a unique name
+      billing_mode = "PAY_PER_REQUEST"
+      hash_key     = "LockID"
+
+      attribute {
+        name = "LockID"
+        type = "S"
+      }
+
+      tags = {
+        Name        = "Terraform State Lock Table"
+        Environment = "Dev"
+      }
+    }
