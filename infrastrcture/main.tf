a750ecadcfae4e8fdaa4df3ce946bdb61d10562f
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-24 10:38:37 +0000

    Updated main.tf

--- infrastrcture/main.tf
+++ infrastrcture/main.tf
@@ -1,79 +1,3 @@
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "eks-vpc"
-  }
-}
-
-resource "aws_subnet" "subnet_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/20"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "eks-subnet-1"
-  }
-}
-
-resource "aws_subnet" "subnet_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.16.0/20"
-  availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "eks-subnet-2"
-  }
-}
-
-resource "aws_subnet" "subnet_3" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.32.0/20"
-  availability_zone       = "us-east-1d"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "eks-subnet-3"
-  }
-}
-
-resource "aws_internet_gateway" "internet_gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "route_table" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gw.id
-  }
-
-  route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = "local"
-  }
-}
-
-resource "aws_route_table_association" "asubnet_1_association" {
-  subnet_id      = aws_subnet.subnet_1.id
-  route_table_id = aws_route_table.route_table.id
-}
-
-resource "aws_route_table_association" "asubnet_2_association" {
-  subnet_id      = aws_subnet.subnet_2.id
-  route_table_id = aws_route_table.route_table.id
-}
-
-resource "aws_route_table_association" "asubnet_3_association" {
-  subnet_id      = aws_subnet.subnet_3.id
-  route_table_id = aws_route_table.route_table.id
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
@@ -81,20 +5,34 @@ module "eks" {
   name               = "my-cluster-eks"
   kubernetes_version = "1.33"
 
-  endpoint_public_access                  = true
+  endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
-  # Disable CloudWatch log group creation
   create_cloudwatch_log_group = false
-
-  compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
 
   vpc_id                   = aws_vpc.main.id
   subnet_ids               = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
   control_plane_subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+
+  # Add managed node group
+  managed_node_groups = {
+    general-purpose = {
+      desired_capacity = 2
+      min_capacity     = 1
+      max_capacity     = 3
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+
+      labels = {
+        role = "general"
+      }
+
+      tags = {
+        Name = "eks-node-group"
+      }
+    }
+  }
 
   tags = {
     Environment = "dev"
