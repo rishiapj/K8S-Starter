3a3ccee73d44788ae43b4074ffbaa527916f0996
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-24 23:49:06 +0000

    Updated providers.tf

--- infrastrcture/providers.tf
+++ infrastrcture/providers.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = ">= 1.3.0"
-
-  backend "s3" {
-    bucket         = "rishi-terraform-bucket-985809756777"
-    key            = "rishi/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0" # Use the latest stable version
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
