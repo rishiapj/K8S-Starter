7a747fd7e9b1fb72d67daaa804b43e3c86f27c1f
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-24 23:49:47 +0000

    Updated outputs.tf

--- infrastrcture/outputs.tf
+++ infrastrcture/outputs.tf
@@ -1,11 +1,23 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "cluster_id" {
+  description = "AWS EKS Cluster ID"
+  value       = module.eks.cluster_id
 }
 
-output "subnet_ids" {
-  value = [
-    aws_subnet.subnet_1.id,
-    aws_subnet.subnet_2.id,
-    aws_subnet.subnet_3.id
-  ]
+output "cluster_endpoint" {
+  description = "AWS EKS Cluster Endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID of the control plane in the cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "region" {
+  description = "AWS region"
+  value       = var.aws_region
+}
+
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
 }
