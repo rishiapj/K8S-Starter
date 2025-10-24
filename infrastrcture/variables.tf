d05f0b5791c485ed8d5eb3ea82e775ad03ffb78d
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-24 23:29:52 +0000

    Updated variables.tf

--- infrastrcture/variables.tf
+++ infrastrcture/variables.tf
@@ -1,3 +1,24 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  default     = "10.0.0.0/16"
+}
+
+variable "eks_name" {
+  description = "AWS EKS Cluster name"
+  type        = string
+}
+
+variable "sg_name" {
+  description = "Security group name"
+  default     = "aws-eks-sg"
 }
