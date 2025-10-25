e315f203ee1b22866a98cd2123628bf0ff395333
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 08:05:59 +0000

    Updated main.tf

--- infrastrcture/main.tf
+++ infrastrcture/main.tf
@@ -10,6 +10,8 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
  
+  cluster_endpoint_public_access  = true
+
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
