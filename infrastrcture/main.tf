13ea23bc2310b99eb97134469efb259752e97cb3
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 07:28:02 +0000

    Updated main.tf

--- infrastrcture/main.tf
+++ infrastrcture/main.tf
@@ -1,29 +1,3 @@
-
-
-variable "existing_kms_key_arn" {
-  type        = string
-  default     = ""
-  description = "Provide an existing KMS key ARN if available. Leave empty to create a new one."
-}
-
-# Create KMS key only if no existing ARN is provided
-resource "aws_kms_key" "eks_key" {
-  count               = var.existing_kms_key_arn == "" ? 1 : 0
-  enable_key_rotation = true
-  description         = var.eks_name
-}
-
-resource "aws_kms_alias" "eks_alias" {
-  count        = var.existing_kms_key_arn == "" ? 1 : 0
-  name         = "alias/eks/${var.eks_name}"
-  target_key_id = aws_kms_key.eks_key[0].key_id
-}
-
-# Local to determine which ARN to use
-locals {
-  kms_key_arn = var.existing_kms_key_arn != "" ? var.existing_kms_key_arn : aws_kms_key.eks_key[0].arn
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
