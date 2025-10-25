94f01e332178bbb881c08df4e3bb2c2960d62962
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 04:41:09 +0000

    Deleted kms.tf

--- infrastrcture/kms.tf
+++
@@ -1,9 +0,0 @@
-data "aws_kms_alias" "existing" {
-  name = "alias/eks/${var.eks_name}"
-}
-
-resource "aws_kms_key" "eks_key" {
-  count = length(data.aws_kms_alias.existing.id) == 0 ? 1 : 0
-  enable_key_rotation = true
-  description         = var.eks_name
-}
