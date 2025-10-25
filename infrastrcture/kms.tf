b74fd3f08805f0f91958ffa33e13f5ce866d0664
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 03:43:48 +0000

    Updated kms.tf

--- infrastrcture/kms.tf
+++ infrastrcture/kms.tf
@@ -1,11 +1,11 @@
 resource "aws_kms_key" "eks_key" {
   description             = "KMS key for EKS cluster encryption"
   deletion_window_in_days = 30
-  enable_key_rotation     = true
+  enable_key_rotation     = trueeks_name
 }
 
 resource "aws_kms_alias" "eks_alias" {
-  name          = "alias/eks/${var.eks_cluster_name}"
+  name          = "alias/eks/${var.eks_name}"
   target_key_id = aws_kms_key.eks_key.key_id
 
   lifecycle {
