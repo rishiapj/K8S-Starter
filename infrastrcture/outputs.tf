d70bb31ac9b5f75aea2693cbc083acd076f78bc3
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 12:15:22 +0000

    Updated outputs.tf

--- infrastrcture/outputs.tf
+++ infrastrcture/outputs.tf
@@ -21,3 +21,8 @@ output "region" {
 output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
 }
+
+
+output "pipeline_role_access_entry" {
+  value = aws_eks_access_entry.pipeline_role_access.principal_arn
+}
