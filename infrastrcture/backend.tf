5ff304e59794562230a8ff3a56d038f918f58a6e
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 06:27:32 +0000

    Added backend.tf

---
+++ infrastrcture/backend.tf
@@ -0,0 +1,6 @@
+ backend "s3" {
+    bucket         = "rishi-terraform-bucket-985809756777"
+    key            = "rishi/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
