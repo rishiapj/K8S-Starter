9d20b2e42066c60a91c08bf4cf29d3ade4269189
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 04:34:56 +0000

    Updated kms.tf

--- infrastrcture/kms.tf
+++ infrastrcture/kms.tf
@@ -1,19 +1,9 @@
-
-variable "existing_kms_key_arn" {
-  type        = string
-  default     = ""
-  description = "Provide an existing KMS key ARN if available. Leave empty to create a new one."
+data "aws_kms_alias" "existing" {
+  name = "alias/eks/${var.eks_name}"
 }
 
-# Create KMS key only if no existing ARN is provided
 resource "aws_kms_key" "eks_key" {
-  count               = var.existing_kms_key_arn == "" ? 1 : 0
+  count = length(data.aws_kms_alias.existing.id) == 0 ? 1 : 0
   enable_key_rotation = true
   description         = var.eks_name
-}
-
-resource "aws_kms_alias" "eks_alias" {
-  count        = var.existing_kms_key_arn == "" ? 1 : 0
-  name         = "alias/eks/${var.eks_name}"
-  target_key_id = aws_kms_key.eks_key[0].key_id
 }
