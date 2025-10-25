0deae7ae89df94b64cf437f9d1a0955665732f2c
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-25 06:27:59 +0000

    Updated terraform.tf

--- infrastrcture/terraform.tf
+++ infrastrcture/terraform.tf
@@ -1,12 +1,4 @@
 terraform {
-  
-  backend "s3" {
-    bucket         = "rishi-terraform-bucket-985809756777"
-    key            = "rishi/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
