79c81f863f49ae8d64aac987882094e9157cb214
Rishikesh Kumar <rishi.zing@gmail.com>
2025-10-27 19:01:41 +0000

    Updated backend.tf

--- eks-tf/eks/backend.tf
+++ eks-tf/eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "dev-rishi-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_loc_files = true
     encrypt        = true
   }
 }
