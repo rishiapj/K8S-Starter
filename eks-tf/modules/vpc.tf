38a386ffe69ddb1e05a0905ecd65ec732bc6c985
rishiapj <37538792+rishiapj@users.noreply.github.com>
2025-10-31 13:41:51 +0530

    Update vpc.tf

--- eks-tf/modules/vpc.tf
+++ eks-tf/modules/vpc.tf
@@ -273,13 +273,15 @@ user_data = <<-EOF
     sudo ./aws/install
 
     # Install kubectl
-    curl -LO "https://dl.k8s.io/release/$(curl -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    chmod +x kubectl
-    mv kubectl /usr/local/bin/
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256" echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
 
     # Verify installations
     aws --version
     kubectl version --client
+    
   EOF
 
   tags = {
